19e2ce44fed45a3ba1a197e021dc631743871211
clonq <ovi.comes@gmail.com>
2015-08-16 11:14:53 -0400

    removed timeout

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 clonq_revo_ui_bootstrap = {
 	init: function(config) {
 		var config = config || {};
-		if(config.load) revo.load({component:config.load})
+		// if(config.load) setTimeout(function() { revo.load({component:config.load}) }, 1000);
+		if(config.load) revo.load({component:config.load});
 		if(config.remove) $(config.remove).remove();
 	}
 }
